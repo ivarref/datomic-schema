749c38d4e0cab913d6b07f4216d93391fe9cd6a0
Ivar Refsdal <ivar.refsdal@nsd.no>
2018-05-07 15:42:33 +0200

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject datomic-schema "0.1.0-SNAPSHOT"
+(defproject datomic-schema "0.1.0"
   :description "Simplified writing of datomic schemas"
   :url "https://github.com/ivarref/datomic-schema"
   :license {:name "Eclipse Public License"
