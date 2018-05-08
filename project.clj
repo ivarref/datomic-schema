88a8e4143f287c1d4a623aafa4bd0f51745586ac
Ivar Refsdal <ivar.refsdal@nsd.no>
2018-05-08 09:27:46 +0200

    Make doc-string optional

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject dato-schema "0.1.1"
+(defproject dato-schema "0.1.2-SNAPSHOT"
   :description "Simplified writing of datomic schemas"
   :url "https://github.com/ivarref/datomic-schema"
   :license {:name "Eclipse Public License"
