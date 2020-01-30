b7824ab97f175a1f96ce16b8c1bbe6998b6a54d1
Ivar Refsdal <ivar.refsdal@nsd.no>
2020-01-30 11:53:32 +0100

    Support tuples #6

--- src/datomic_schema/core.clj
+++ src/datomic_schema/core.clj
@@ -1,4 +1,5 @@
-(ns datomic-schema.core)
+(ns datomic-schema.core
+  (:require [clojure.string :as str]))
 
 ;; Schema literals
 ;; ---------------
@@ -24,6 +25,36 @@
   `(when-not ~f
      (throw (AssertionError. (schema-problem ~flavor ~emit)))))
 
+(def ^:private tuple-scalars (into (sorted-set) #{:db.type/bigdec :db.type/bigint :db.type/boolean :db.type/double
+                                                  :db.type/instant :db.type/keyword :db.type/long :db.type/string
+                                                  :db.type/symbol :db.type/ref :db.type/uri :db.type/uuid}))
+
+(defn- tuple-scalar [attr]
+  (or (some #{attr} tuple-scalars)
+      (some #{(keyword "db.type" (name attr))} tuple-scalars)))
+
+(defn- ns-keyword? [kw]
+  (and (keyword? kw)
+       (some? (namespace kw))))
+
+(defn- tuple-attrs [attrs]
+  (if (= 1 (count attrs))
+    (some->> (tuple-scalar (first attrs))
+             (assoc {} :db/tupleType)) ; Homogeneous Tuples
+    (when (and (>= (count attrs) 2)
+               (<= (count attrs) 8))
+      (cond (every? tuple-scalar attrs)
+            (assoc {} :db/tupleTypes (mapv tuple-scalar attrs)) ; Heterogeneous Tuples
+
+            (some tuple-scalar attrs)
+            nil
+
+            (every? ns-keyword? attrs)
+            (assoc {} :db/tupleAttrs (mapv identity attrs)) ; Composite Tuples
+
+            :else
+            nil))))
+
 (defn- parse-schema-vec
   [s-vec]
   (let [doc-string (when (string? (last s-vec)) (last s-vec))
@@ -32,37 +63,55 @@
         opt-toggles (take-while keyword? (drop 3 s-vec))]
     (cond (and (= 2 (count s-vec))
                (= :enum (last s-vec)))
-          {:db/ident (first s-vec)}
+          {:db/ident ident}
 
           (not (contains? accepted-cards card))
           (parse-schema-vec (into [ident :one] (rest s-vec)))
 
-          :else (do
-                  (schema-assert (every? keyword? s-vec)
-                                 "All of _attribute-name_, _cardinality_, _type_, and _toggles_ must be Clojure keywords."
-                                 s-vec)
-                  (schema-assert (some? (namespace ident)) "Ident must have namespace" ident)
-                  (schema-assert (every? #(contains? accepted-schema-toggles %) opt-toggles)
-                                 (str "Short schema toggles must be taken from " accepted-schema-toggles) opt-toggles)
-                  (schema-assert (contains? accepted-kinds kind) (str "The value type must be one of " accepted-kinds) kind)
-                  (schema-assert (contains? accepted-cards card) (str "The cardinality must be one of " accepted-cards) card)
-                  (merge {:db/ident       ident
-                          :db/valueType   (keyword "db.type" (name kind))
-                          :db/cardinality (keyword "db.cardinality" (name card))}
-                         (when doc-string {:db/doc doc-string})
-                         (reduce (fn [m opt]
-                                   (merge m (case opt
-                                              :unique {:db/unique :db.unique/value}
-                                              :identity {:db/unique :db.unique/identity}
-                                              :id {:db/unique :db.unique/identity}
-                                              :index {:db/index true}
-                                              :fulltext {:db/fulltext true
-                                                         :db/index    true}
-                                              :component {:db/isComponent true}
-                                              :no-history {:db/noHistory true}
-                                              nil)))
-                                 {}
-                                 opt-toggles))))))
+          :else
+          (do
+            (schema-assert (ns-keyword? ident) "attribute-name must be a namespaced keyword." s-vec)
+            #_(schema-assert (every? keyword? s-vec)
+                             "All of _attribute-name_, _cardinality_, _type_, and _toggles_ must be Clojure keywords."
+                             s-vec)
+            (schema-assert (every? #(contains? accepted-schema-toggles %) opt-toggles)
+                           (str "Short schema toggles must be taken from " accepted-schema-toggles) opt-toggles)
+            (when (keyword? kind)
+              (schema-assert (contains? accepted-kinds kind) (str "The value type must be one of " accepted-kinds) kind))
+            (when (vector? kind)
+              (schema-assert (tuple-attrs kind) (str "The type of a tuple must be:\n"
+                                                     "* Composite tuple: The value must be a vector of 2-8 keywords naming other attributes.\n"
+                                                     "* Heterogeneous fixed length tuple: The value must be a vector of 2-8 scalar value types: "
+                                                     (str/join ", " (mapv #(str ":" (name %)) tuple-scalars)) "\n"
+                                                     "* Homogeneous variable length tuple: The value must be a vector containing 1 scalar type.")
+                             kind))
+            (schema-assert (contains? accepted-cards card) (str "The cardinality must be one of " accepted-cards) card)
+            (merge {:db/ident       ident
+                    :db/cardinality (keyword "db.cardinality" (name card))}
+                   (when (keyword? kind)
+                     {:db/valueType (keyword "db.type" (name kind))})
+                   (when (vector? kind)
+                     (merge {:db/valueType :db.type/tuple}
+                            (tuple-attrs kind)))
+                   (when doc-string
+                     {:db/doc doc-string})
+                   (reduce (fn [m opt]
+                             (merge m (case opt
+                                        :unique {:db/unique :db.unique/value}
+                                        :identity {:db/unique :db.unique/identity}
+                                        :id {:db/unique :db.unique/identity}
+                                        :index {:db/index true}
+                                        :fulltext {:db/fulltext true
+                                                   :db/index    true}
+                                        :component {:db/isComponent true}
+                                        :no-history {:db/noHistory true}
+                                        nil)))
+                           {}
+                           opt-toggles))))))
+
+(comment
+  (parse-schema-vec [:hello/world [:keyword]]))
+
 
 (defn schema-tx [form]
   (schema-assert (vector? form) "The top level must be a vector." form)
--- test/datomic_schema/core_test.clj
+++ test/datomic_schema/core_test.clj
@@ -6,74 +6,96 @@
   (are [case input expected] (testing case (= expected (read-string input)))
                              "One attribute"
                              "#datomic/schema[[:entity/attribute :one :long \"A docstring\"]]"
-                             [{:db/ident              :entity/attribute
-                               :db/valueType          :db.type/long
-                               :db/cardinality        :db.cardinality/one
-                               :db/doc                "A docstring"}]
+                             [{:db/ident       :entity/attribute
+                               :db/valueType   :db.type/long
+                               :db/cardinality :db.cardinality/one
+                               :db/doc         "A docstring"}]
 
                              "Two attributes"
                              "#datomic/schema[[:e/a1 :one :long \"\"] [:e/a2 :many :string \"docstring 2\"]]"
-                             [{:db/ident              :e/a1
-                               :db/valueType          :db.type/long
-                               :db/cardinality        :db.cardinality/one
-                               :db/doc                ""}
-                              {:db/ident              :e/a2
-                               :db/valueType          :db.type/string
-                               :db/cardinality        :db.cardinality/many
-                               :db/doc                "docstring 2"}]
+                             [{:db/ident       :e/a1
+                               :db/valueType   :db.type/long
+                               :db/cardinality :db.cardinality/one
+                               :db/doc         ""}
+                              {:db/ident       :e/a2
+                               :db/valueType   :db.type/string
+                               :db/cardinality :db.cardinality/many
+                               :db/doc         "docstring 2"}]
 
                              "One toggle"
                              "#datomic/schema[[:e/a :one :long :identity \"Doc\"]]"
-                             [{:db/ident              :e/a
-                               :db/valueType          :db.type/long
-                               :db/cardinality        :db.cardinality/one
-                               :db/unique             :db.unique/identity
-                               :db/doc                "Doc"}]
+                             [{:db/ident       :e/a
+                               :db/valueType   :db.type/long
+                               :db/cardinality :db.cardinality/one
+                               :db/unique      :db.unique/identity
+                               :db/doc         "Doc"}]
 
                              "Several toggles"
                              "#datomic/schema[[:e/a :one :string :identity :index :component :no-history :fulltext \"Doc\"]]"
-                             [{:db/ident              :e/a
-                               :db/valueType          :db.type/string
-                               :db/cardinality        :db.cardinality/one
-                               :db/index              true
-                               :db/isComponent        true
-                               :db/noHistory          true
-                               :db/fulltext           true
-                               :db/unique             :db.unique/identity
-                               :db/doc                "Doc"}]
+                             [{:db/ident       :e/a
+                               :db/valueType   :db.type/string
+                               :db/cardinality :db.cardinality/one
+                               :db/index       true
+                               :db/isComponent true
+                               :db/noHistory   true
+                               :db/fulltext    true
+                               :db/unique      :db.unique/identity
+                               :db/doc         "Doc"}]
 
                              "Docstring is optional"
                              "#datomic/schema[[:e/a :one :string]]"
-                             [{:db/ident :e/a
-                               :db/valueType          :db.type/string
-                               :db/cardinality        :db.cardinality/one}]
+                             [{:db/ident       :e/a
+                               :db/valueType   :db.type/string
+                               :db/cardinality :db.cardinality/one}]
 
                              "Cardinality is optional"
                              "#datomic/schema[[:e/a :string]]"
-                             [{:db/ident :e/a
-                               :db/valueType          :db.type/string
-                               :db/cardinality        :db.cardinality/one}]
+                             [{:db/ident       :e/a
+                               :db/valueType   :db.type/string
+                               :db/cardinality :db.cardinality/one}]
 
                              "Cardinality is optional and works with toggles"
                              "#datomic/schema[[:e/a :string :unique]]"
-                             [{:db/ident :e/a
-                               :db/valueType          :db.type/string
-                               :db/cardinality        :db.cardinality/one
-                               :db/unique             :db.unique/value}]
+                             [{:db/ident       :e/a
+                               :db/valueType   :db.type/string
+                               :db/cardinality :db.cardinality/one
+                               :db/unique      :db.unique/value}]
 
                              ":id shorthand"
                              "#datomic/schema[[:e/a :string :id]]"
-                             [{:db/ident :e/a
-                               :db/valueType          :db.type/string
-                               :db/cardinality        :db.cardinality/one
-                               :db/unique             :db.unique/identity}]
+                             [{:db/ident       :e/a
+                               :db/valueType   :db.type/string
+                               :db/cardinality :db.cardinality/one
+                               :db/unique      :db.unique/identity}]
+
+                             "Composite Tuples"
+                             "#datomic/schema[[:semester/year+season [:semester/year :semester/season] :id]]"
+                             [{:db/ident       :semester/year+season
+                               :db/valueType   :db.type/tuple
+                               :db/tupleAttrs  [:semester/year :semester/season]
+                               :db/cardinality :db.cardinality/one
+                               :db/unique      :db.unique/identity}]
+
+                             "Heterogeneous Tuples"
+                             "#datomic/schema[[:player/location [:long :long]]]"
+                             [{:db/ident       :player/location
+                               :db/valueType   :db.type/tuple
+                               :db/tupleTypes  [:db.type/long :db.type/long]
+                               :db/cardinality :db.cardinality/one}]
+
+                             "Homogeneous Tuples"
+                             "#datomic/schema[[:db/tupleAttrs [:keyword]]]"
+                             [{:db/ident       :db/tupleAttrs
+                               :db/valueType   :db.type/tuple
+                               :db/tupleType   :db.type/keyword
+                               :db/cardinality :db.cardinality/one}]
 
                              "Enum support"
                              "#datomic/schema[[:e/a :enum]]"
                              [{:db/ident :e/a}]
 
                              "Shorthand works"
-                             "#d/s[[:e/a :enum]]"
+                             "#d/schema[[:e/a :enum]]"
                              [{:db/ident :e/a}])
   (are [bad-input] (thrown? Throwable (read-string bad-input))
                    "#datomic/schema[[:e/a :one-is-the-lonliest-number :long \"doc\"]]"
